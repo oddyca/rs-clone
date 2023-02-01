df0a90da199a4ecd139616b299f40dc7123de38d
Roman Maklakov <mclakov@gmail.com>
2023-02-01 18:35:05 +0300

    feat: add prett and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <div>RS-CLONE</div>
     </div>
   );
 }
