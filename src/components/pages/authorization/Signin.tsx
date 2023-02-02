6c746183eca6e39d89a41ef4e36074c02553f162
Siarhei Prakapovich <prokser27@icloud.com>
2023-02-02 19:52:49 +0300

    fix: hook syntax in signin.tsx

--- src/components/pages/authorization/Signin.tsx
+++ src/components/pages/authorization/Signin.tsx
@@ -1,46 +1,49 @@
-import React, { useState } from 'react'
+import { useState } from "react"
 import { Link } from "react-router-dom";
 
 import "../../../style/auth-modal.css";
 import Controller from "../../../lib/Controller";
 
-const APP_CONTROLLER = new Controller();
-const [userEmail, setEmail] = useState('');
-const [userPassword, setPassword] = useState('');
-
-function signInVerification() {
-    console.log(userEmail, userPassword)
-    return undefined;
-}
-
 function SignIn() {
-  return (
-    <div className="auth-window">
-        <div className="auth-modal">
-            <div className="hero-shot"></div>
-            <div className="auth-block">
-                <div className="auth-wrapper">
-                    <h3>Sign in</h3>
-                    <form className="auth-form" onSubmit={() => signInVerification()}>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            required
-                            onChange={(e) => setEmail(e.target.value)}>
-                        </input>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            required
-                            onChange={(e) => setPassword(e.target.value)}>
-                        </input>
-                        <button /*onClick={(e) => e.preventDefault()}*/>Sign in</button>
-                    </form>
-                    <p>Or <Link to="/signup">Create new account</Link></p>
+    const APP_CONTROLLER = new Controller();
+    const [userEmail, setEmail] = useState('');
+    const [userPassword, setPassword] = useState('');
+
+    function signInVerification() {
+        console.log(userEmail, userPassword)
+        return undefined;
+    }
+
+    return (
+        <div className="auth-window">
+            <div className="auth-modal">
+                <div className="hero-shot"></div>
+                <div className="auth-block">
+                    <div className="auth-wrapper">
+                        <h3>Sign in</h3>
+                        <form className="auth-form" onSubmit={(e) => {
+                            e.preventDefault();
+                            signInVerification();
+                            }}>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                required
+                                onChange={(e) => setEmail(e.target.value)}>
+                            </input>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                required
+                                onChange={(e) => setPassword(e.target.value)}>
+                            </input>
+                            <button>Sign in</button>
+                        </form>
+                        <p>Or <Link to="/signup">Create new account</Link></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 export default SignIn
