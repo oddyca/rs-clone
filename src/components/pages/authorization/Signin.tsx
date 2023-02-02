c5513f01532c6df3a30d976c8bb83814a46e9b9c
Siarhei Prakapovich <prokser27@icloud.com>
2023-02-02 21:55:28 +0300

    feat: add signup form

--- src/components/pages/authorization/Signin.tsx
+++ src/components/pages/authorization/Signin.tsx
@@ -1,17 +1,33 @@
 import { useState } from "react"
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "../../../style/auth-modal.css";
 import Controller from "../../../lib/Controller";
 
-function SignIn() {
+interface UsernamePassword {
+    [username: string]: string
+}
+
+export default function SignIn() {
     const APP_CONTROLLER = new Controller();
+    const fetchedData = APP_CONTROLLER.loadData(); // only first user. Must be an array of users
+
     const [userEmail, setEmail] = useState('');
     const [userPassword, setPassword] = useState('');
+    const navigate = useNavigate();
 
-    function signInVerification() {
-        console.log(userEmail, userPassword)
-        return undefined;
+    const  signInVerification = () => {
+        const userNamePass: UsernamePassword = {
+            'username': fetchedData.USER_NAME,
+            'password': fetchedData.USER_PASSWORD
+        }
+
+        if (userEmail === userNamePass['username'] && userPassword === userNamePass['password']) {
+            navigate('/');
+        } else {
+            alert('wrong user-password');
+            return undefined
+        }
     }
 
     return (
@@ -22,28 +38,29 @@ function SignIn() {
                     <div className="auth-wrapper">
                         <h3>Sign in</h3>
                         <form className="auth-form" onSubmit={(e) => {
-                            e.preventDefault();
-                            signInVerification();
-                            }}>
+                            e.preventDefault()
+                            signInVerification()}}>
                             <input
-                                type="email"
-                                placeholder="Email"
+                                type="text"
+                                placeholder="Username"
+                                className="form-input"
                                 required
                                 onChange={(e) => setEmail(e.target.value)}>
                             </input>
                             <input
                                 type="password"
                                 placeholder="Password"
+                                className="form-input"
                                 required
                                 onChange={(e) => setPassword(e.target.value)}>
                             </input>
-                            <button>Sign in</button>
+                            <a className="forgot-password">Forgot password?</a>
+                            <button className="button auth-button">Sign in</button>
                         </form>
-                        <p>Or <Link to="/signup">Create new account</Link></p>
+                        <p>Or <a className="signup-link" onClick={()=> navigate('/signup')}>Create new account</a></p>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-export default SignIn
--- src/components/pages/authorization/Signup.tsx
+++ src/components/pages/authorization/Signup.tsx
@@ -1,40 +1,82 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 import "../../../style/auth-modal.css";
+import Controller from "../../../lib/Controller";
 
-function SignUp() {
-  return (
-    <div className="auth-window">
-        <div className="auth-modal">
-            <div className="hero-shot"></div>
-            <div className="auth-block">
-                <div className="auth-wrapper">
-                    <h3>Sign in</h3>
-                    <form className="auth-form">
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            required>
-                        </input>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            required>
-                        </input>
-                        <input
-                            type="password"
-                            placeholder="Repeat password"
-                            required>
-                        </input>
-                        <Link to="/signin">
-                            <button>Sign up</button>
-                        </Link>
-                    </form>
+interface UsernamePassword {
+    [username: string]: string
+}
+
+export default function SignUp() {
+    const APP_CONTROLLER = new Controller();
+    const fetchedData = APP_CONTROLLER.loadData(); // only first user. Must be an array of users
+
+    const [userEmail, setEmail] = useState('');
+    const [userPassword, setPassword] = useState('');
+    const [userRepeatPassword, setRepeatPassword] = useState('');
+    const navigate = useNavigate();
+
+    const  signInVerification = () => {
+        const userNamePass: UsernamePassword = {
+            'username': fetchedData.USER_NAME,
+            'password': fetchedData.USER_PASSWORD
+        }
+
+        if (userEmail // will check if it exists already 
+            && (userRepeatPassword === userPassword)) {
+            navigate('/signin');
+        } else {
+            alert('password don\'t match');
+            return undefined
+        }
+    }
+
+    return (
+        <div className="auth-window">
+            <div className="auth-modal">
+                <div className="hero-shot"></div>
+                <div className="auth-block">
+                    <div className="auth-wrapper">
+                        <h3>Sign in</h3>
+                        <form className="auth-form" onSubmit={(e) => {
+                            e.preventDefault()
+                            signInVerification()}}>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                className="form-input"
+                                required
+                                onChange={(e) => setEmail(e.target.value)}>
+                            </input>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="form-input"
+                                required
+                                onChange={(e) => setPassword(e.target.value)}>
+                            </input>
+                            <input
+                                type="password"
+                                placeholder="Repeat password"
+                                className="form-input"
+                                required
+                                onChange={(e) => setRepeatPassword(e.target.value)}>
+                            </input>
+                            <label htmlFor="privacy">
+                            <input
+                                type="checkbox"
+                                id="privacy"
+                                required>
+                            </input>
+                            I agree with the Privacy policy
+                            </label>
+                            
+                            <button className="button auth-button">Sign up</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
-export default SignUp
