15ef026ae18ca25efec8e0d62ae710215f097ef4
shzfas <yuraka200486@gmail.com>
2023-02-02 17:30:50 +0600

    feat: header-styles

--- src/components/widgets/header/Header.tsx
+++ src/components/widgets/header/Header.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css"
 
 function Header(props: any) {
   const { title, userWorkSpace } = props;
 
   return (
-    <div className="Header">
+    <header className="header">
       {userWorkSpace.map((d: any, index: number) => {
         /* console.log(d) */
         return (
-          <Link
-          className="link" to={`/workspace-${index}`}>{d.WORKSPACE_TITLE}</Link>
-        )
+          <Link className="link" to={`/workspace-${index}`}>
+            {d.WORKSPACE_TITLE}
+          </Link>
+        );
       })}
       <div>{title}</div>
-    </div>
+    </header>
   );
-};
+}
 
 export default Header;
